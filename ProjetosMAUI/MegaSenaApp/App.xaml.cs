4460a2ecc5bbcdb1bca33dd926d003cf47eabf26
vascsx <vasczn@gmail.com>
2023-12-27 20:08:03 -0300

    first commit

---
+++ ProjetosMAUI/MegaSenaApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MegaSenaApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ ProjetosMAUI/MegaSenaApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MegaSenaApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
